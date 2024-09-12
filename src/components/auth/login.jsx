b82deac5126eeff725d5d9130949d54dc28ad54d
Getteruz <uzgetter@gmail.com>
2024-09-12 23:41:52 +0500

    first commit

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -15,9 +15,6 @@ const Auth = () => {
         password,
       });
       localStorage.setItem('authToken', response.data.token);
-      console.log('Token saved:', response.data.token);
-
-      window.location.reload()
       navigate('/products');
     } catch (error) {
       setErrorMessage('Login failed, please check your credentials.');
@@ -26,22 +23,47 @@ const Auth = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={login}>Login</button>
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
+        <h2 className="text-3xl font-bold mb-6 text-center">Login</h2>
+        {errorMessage && <p className="mb-4 text-red-500 text-center">{errorMessage}</p>}
+        <form onSubmit={(e) => { e.preventDefault(); login(); }}>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-700 text-sm font-semibold mb-2">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="border border-gray-300 p-3 w-full rounded-lg"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-semibold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-300 p-3 w-full rounded-lg"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-3 w-full rounded-lg font-semibold hover:bg-blue-600 transition"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
